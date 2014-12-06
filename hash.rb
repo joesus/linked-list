16af3b8a2ca2854c5eea4dd88d2372e8e40d390f
Joe Susnick <jsusnick@itriagehealth.com>
2014-12-06 16:26:12 -0700

    Changed structure to use linked_list as base.

--- hash.rb
+++ hash.rb
@@ -1,31 +1,31 @@
 require 'pry-nav'
-require_relative 'array'
+require_relative 'linked_list'
 
 class LLHash
-  attr_accessor :llarray, :default
+  attr_accessor :linked_list, :default
 
-  def initialize(default=nil)
+  def initialize(default=nil, *args)
     self.default = default
-    self.llarray = LLArray.new
+    self.linked_list = LinkedList.new
   end
 
   def [](key)
-    self.llarray.linked_list.get_by_key(key).contents
+    self.linked_list.get_by_key(key).contents
   rescue NoMethodError
     self.default
   end
 
   def []=(key, contents)
     link = Link.new(contents: contents, key: key)
-    self.llarray.linked_list.add(link)
+    self.linked_list.add(link)
   end
 
   def to_s
-  	if self.llarray.empty?
+  	if self.linked_list.first_link.nil?
   		"{}"
   	else
   		string = ""
-  		self.llarray.linked_list.each do |link|
+  		self.linked_list.each do |link|
         string << "#{link.key}: '#{link.contents}', "
       end
       2.times { string.chop! }
