068c5091a7fc993a7f1a02a7096d8e959fbf819c
Joe Susnick <joesusnick@gmail.com>
2014-12-15 16:55:48 -0700

    updated hash to use MD5

--- hash.rb
+++ hash.rb
@@ -1,40 +1,61 @@
+require 'digest'
 require 'pry-nav'
 require_relative 'linked_list'
 
 class LLHash
-  attr_accessor :linked_list, :default
+  attr_accessor :linked_list, :values_array, :default
 
   def initialize(*args)
-  	if args.length < 2
-  		self.default = args.first
-  	end
-  	self.linked_list = LinkedList.new
-  	args.each_slice(2) do |slice|
-  		self.linked_list.add(Link.new(key: slice[0], contents: slice[1]))
-  	end
+    self.values_array = []
+    if args.length < 2
+      self.default = args.first
+    end
+    args.each_slice(2) do |slice|
+      add_pair(slice[0], slice[1])
+    end
+  end
+
+  def []=(key, value)
+    add_pair(key, value)
   end
 
   def [](key)
-    self.linked_list.get_by_key(key).contents
-  rescue NoMethodError
-    self.default
-  end
-
-  def []=(key, contents)
-    link = Link.new(contents: contents, key: key)
-    self.linked_list.add(link)
+    index = Digest::MD5.hexdigest(key).hex % 10
+    if self.values_array[index] && self.values_array[index].size > 1
+      self.values_array[index].get_by_key(key).contents
+    elsif self.values_array[index]
+      self.values_array[index].first.contents
+    else
+      self.default
+    end
   end
 
   def to_s
-  	if self.linked_list.first_link.nil?
-  		"{}"
-  	else
-  		string = ""
-  		self.linked_list.each do |link|
-        string << "#{link.key}: '#{link.contents || 'nil'}', "
+    if self.values_array.empty?
+      "{}"
+    else
+      string = ""
+      self.values_array.each do |linked_list|
+        unless linked_list.nil?
+          linked_list.each do |link|
+            string << "#{link.key}: '#{link.contents || 'nil'}', " rescue nil
+          end
+        end
       end
       2.times { string.chop! }
       "{#{string}}"
+    end
+  end
+
+  private
+
+  def add_pair(key, value)
+    link = Link.new(key: key, contents: value)
+    index = Digest::MD5.hexdigest(key).hex % 10
+    if self.values_array[index].nil?
+      self.values_array[index] = LinkedList.new(link)
+    else
+      self.values_array[index].add(link)
     end
   end
 end
--- hash_test.rb
+++ hash_test.rb
@@ -5,29 +5,38 @@ class HashTest < MiniTest::Unit::TestCase
 
   def setup
     @hash = LLHash.new
+    @hash["name"] = "Joe"
     @meal = LLHash.new("appetizer", "bacon-stuff", "salad", "waldorf", "entree", "steak", "side", "potatoes", "dessert", "cakes")
-    @sandwich = LLHash.new("bread", "wheat", "meat", "turkey", "mustard")
   end
 
-  def test_hash_values_default_to_nil
-    assert_equal nil, @hash["anything"]
+  def test_default_value
+    assert_equal nil, @hash["default"]
   end
 
-  def test_can_set_default_value
-    @hash = LLHash.new("default")
-    assert_equal "default", @hash["anything"]
-  end
-
-  def test_to_s_works_with_empty_hash
-    assert_equal "{}", @hash.to_s
+  def test_to_s
+    assert_equal "{name: 'Joe'}", @hash.to_s
   end
 
   def test_to_s_works_with_full_hash
-    assert_equal "{appetizer: 'bacon-stuff', salad: 'waldorf', entree: 'steak', side: 'potatoes', dessert: 'cakes'}", @meal.to_s
+    assert_equal "{entree: 'steak', salad: 'waldorf', dessert: 'cakes', appetizer: 'bacon-stuff', side: 'potatoes'}", @meal.to_s
   end
 
-  def test_setting_hash_with_odd_values_defaults_last_value_to_nil
-  	@sandwich = LLHash.new("bread", "wheat", "meat", "turkey", "mustard")
-  	assert_equal "{bread: 'wheat', meat: 'turkey', mustard: 'nil'}", @sandwich.to_s
+  def test_duplicate_hash_keys_are_retrievable
+    @hash["asdf"] = "123"
+    @hash["asdf3"] = "abc"
+    assert_equal "123", @hash["asdf"]
+    assert_equal "abc", @hash["asdf3"]
+  end
+
+  def test_initializing_with_even_items_works_as_expected
+    @sandwich = LLHash.new("bread", "wheat", "meat", "turkey", "condiment", "mustard")
+    assert_equal "turkey", @sandwich["meat"]
+    assert_equal "wheat", @sandwich["bread"]
+    assert_equal "{bread: 'wheat', condiment: 'mustard', meat: 'turkey'}", @sandwich.to_s
+  end
+
+  def test_initializing_with_odd_values_defaults_last_value_to_nil
+    @sandwich = LLHash.new("bread", "wheat", "meat", "turkey", "mustard")
+    assert_equal "{mustard: 'nil', bread: 'wheat', meat: 'turkey'}", @sandwich.to_s
   end
 end
